994be4425b0b3169fa7f24066cf7cf4e73711a88
Kalyan-71 <164600184+Kalyan-71@users.noreply.github.com>
2025-07-21 21:02:56 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,10 @@
+const name = "kalyan"
+const count = 20
+
+// console.log(name + count + " Value")//old way
+
+//string interpolation
+// console.log(`hello my name is ${name} and age is ${count}`)
+
+///try in browser console
+let gameName = new String('8 ball pool') // object 
